f4ac667d43ea2c81a60fd0e03ab2ce05a17eed59
Oleg-Hahunov <ovhahunov@gmail.com>
2022-04-19 22:22:52 +0300

    finaly

--- main.py
+++ main.py
@@ -5,53 +5,49 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/")
-def profile():
-    candidate = f'<pre>\n'
-    with open('candidate.json', 'r', encoding="utf8") as File:
-        temp = json.load(File)
-        for i in range(len(temp)):
-            candidate += f'Имя кандидата - {temp[i]["name"]}\n'
-            candidate += f'позиция кандидата - {temp[i]["position"]}\n'
-            candidate += f'Навыки - {temp[i]["skills"]}\n'
-            candidate += f'\n'
+def read_json():  # считывание джейсон файла с базой кандидатов
 
-    return candidate + '<pre>'
+
+    with open('candidate.json', 'r', encoding="utf8") as File:
+        return json.load(File)
+
+
+@app.route("/")
+def profile():  # основной список кандидатов
+    temp = read_json()
+    candidate = f'<pre>\n'
+    for i in range(len(temp)):
+        candidate += f'Имя кандидата - {temp[i]["name"]}\n'
+        candidate += f'позиция кандидата - {temp[i]["position"]}\n'
+        candidate += f'Навыки - {temp[i]["skills"]}\n'
+        candidate += f'\n<pre>'
+    return candidate
 
 
 @app.route('/candidates/<int:id>')
-def personal_page(id):
-    with open('candidate.json', 'r', encoding="utf8") as File:
-        temp = json.load(File)
-        personal_p = f'<img src= "{temp[id - 1]["picture"]}"\n>'
-        personal_p += f'<pre>\n'
-        personal_p += f'Имя кандидата - {temp[id - 1]["name"]}\n'
-        personal_p += f'позиция кандидата - {temp[id - 1]["position"]}\n'
-        personal_p += f'Навыки - {temp[id - 1]["skills"]}\n'
-        personal_p += f'\n'
-
-    return personal_p + '<pre>'
+def personal_page(id):# модуль возвращает информацию о каждом кандидате с аватаркой
+    temp = read_json()
+    personal_p = f'<img src= "{temp[id - 1]["picture"]}"\n>'
+    personal_p += f'<pre>\n'
+    personal_p += f'Имя кандидата - {temp[id - 1]["name"]}\n'
+    personal_p += f'позиция кандидата - {temp[id - 1]["position"]}\n'
+    personal_p += f'Навыки - {temp[id - 1]["skills"]}\n'
+    personal_p += f'\n<pre>'
+    return personal_p
 
 
-@app.route('/skills/<str:skills>')
-def search_skills():
-    with open('candidate.json', 'r', encoding="utf8") as File:
-        temp = json.load(File)
-        candidate = f'<pre>\n'
+@app.route('/skills/<skills>')
+def search_skills(skills): # модуль возвращает список кандидатов подходящих по указанному навыку
+    temp = read_json()
+    candidate = f'<pre>\n'
     for i in range(len(temp)):
-        if skills in temp[i]['skills']:
+        skills_temp = temp[i]['skills']
+        skills_temp.split(', ')
+        if skills in skills_temp:
             candidate += f'Имя кандидата - {temp[i]["name"]}\n'
             candidate += f'позиция кандидата - {temp[i]["position"]}\n'
-            candidate += f'Навыки - {temp[i]["skills"]}\n'
-        return candidate + '<pre>'
+            candidate += f'Навыки - {temp[i]["skills"]}\n<pre>'
+    return candidate
 
 
 app.run()
-
-#
-# temp[i].pop('id')
-# temp[i].pop('picture')
-# temp[i].pop('gender')
-# temp[i].pop('age')
-# temp[i]['name'] = 'Имя кандидата - ' + temp[i]['name']
-# temp[i]['position'] = 'позиция кандидата -' + temp[i]['position']
